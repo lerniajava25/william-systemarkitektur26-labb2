05f4ed789c973574295cca12450ea4d61125b6e7
William Matton <williammatton@gmail.com>
2026-09-23 14:28:00 +0200

    Add circular dependency detection to DIContainer

--- src/main/java/org/example/container/DIContainer.java
+++ src/main/java/org/example/container/DIContainer.java
@@ -2,20 +2,32 @@ package org.example.container;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DIContainer {
-    public <T> T getInstanceOfClass(Class<T> type) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        var ctor = type.getConstructors()[0];
-        var params = ctor.getParameterTypes();
-        List<Object> dependencies = new ArrayList<>();
+    private final Set<Class<?>> resolutionPath = new HashSet<>();
 
-        for(Class<?> param : params) {
-            var dependency = getInstanceOfClass(param);
-            dependencies.add(dependency);
+    public <T> T getInstanceOfClass(Class<T> type) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        if (!resolutionPath.add(type)) {
+            throw new IllegalStateException("Circular dependency detected for type: " + type.getName());
         }
 
-        var instance = ctor.newInstance(dependencies.toArray());
-        return type.cast(instance);
+        try {
+            var ctor = type.getConstructors()[0];
+            var params = ctor.getParameterTypes();
+            List<Object> dependencies = new ArrayList<>();
+
+            for(Class<?> param : params) {
+                var dependency = getInstanceOfClass(param);
+                dependencies.add(dependency);
+            }
+
+            var instance = ctor.newInstance(dependencies.toArray());
+            return type.cast(instance);
+        } finally {
+            resolutionPath.remove(type);
+        }
     }
 }
